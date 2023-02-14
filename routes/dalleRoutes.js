96fa75336a4e8ca39647f9ea31e42e684ba294dc
zedaenious <zed.dev.22@gmail.com>
2023-02-14 16:43:14 -0500

    setup cloudinary for storing uploaded created imgs

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -27,7 +27,6 @@ router.route('/').post(async (req, res) => {
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
     res.status(500).send(error?.response.data.error.message);
   }
 });
